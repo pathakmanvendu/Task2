f03252f222f929aa8f08a58e0e2f49005b557906
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-22 12:34:21 +0530

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by manvendu pathak  on 22/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
